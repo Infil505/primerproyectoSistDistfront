5d190243ac9f2ccdba8c99cafd70f953c02d4ea4
infil505 <darwin.silva.gadea@est.una.ac.cr>
2025-10-19 16:04:23 -0600

    mejoras

--- src/router.js
+++ src/router.js
@@ -1,32 +1,66 @@
 import { createRouter, createWebHistory } from "vue-router";
 import { getAuth } from "./api";
 
-import Login from "./pages/Login.vue";
-import EdificiosList from "./pages/EdificiosList.vue";
-import EdificioForm from "./pages/EdificioForm.vue";
+import Home from "./App.vue";
+import ArquitectoForm from "./pages/ArquitectoForm.vue";
+import ArquitectosList from "./pages/ArquitectosList.vue";
 import CiudadesList from "./pages/CiudadesList.vue";
 import CiudadForm from "./pages/CiudadForm.vue";
-import ArquitectosList from "./pages/ArquitectosList.vue";
-import ArquitectoForm from "./pages/ArquitectoForm.vue";
+import EdificioForm from "./pages/EdificioForm.vue";
+import EdificiosList from "./pages/EdificiosList.vue";
+import Login from "./pages/Login.vue";
 import Register from "./pages/Register.vue";
-import Home from "./App.vue";
-
 
 const routes = [
   { path: "/home", name: "Home", component: Home },
   { path: "/", name: "Login", component: Login },
-  { path: "/register", name: "register", component: Register},
-  { path: "/edificios", component: EdificiosList, meta: { requiresAuth: true } },
-  { path: "/edificios/nuevo", component: EdificioForm, meta: { requiresAuth: true } },
-  { path: "/edificios/:id", component: EdificioForm, props: true, meta: { requiresAuth: true } },
+  { path: "/register", name: "register", component: Register },
+  {
+    path: "/edificios",
+    component: EdificiosList,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/edificios/nuevo",
+    component: EdificioForm,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/edificios/:id",
+    component: EdificioForm,
+    props: true,
+    meta: { requiresAuth: true },
+  },
 
   { path: "/ciudades", component: CiudadesList, meta: { requiresAuth: true } },
-  { path: "/ciudades/nuevo", component: CiudadForm, meta: { requiresAuth: true } },
-  { path: "/ciudades/:id", component: CiudadForm, props: true, meta: { requiresAuth: true } },
+  {
+    path: "/ciudades/nuevo",
+    component: CiudadForm,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/ciudades/:id",
+    component: CiudadForm,
+    props: true,
+    meta: { requiresAuth: true },
+  },
 
-  { path: "/arquitectos", component: ArquitectosList, meta: { requiresAuth: true } },
-  { path: "/arquitectos/nuevo", component: ArquitectoForm, meta: { requiresAuth: true } },
-  { path: "/arquitectos/:id", component: ArquitectoForm, props: true, meta: { requiresAuth: true } },
+  {
+    path: "/arquitectos",
+    component: ArquitectosList,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/arquitectos/nuevo",
+    component: ArquitectoForm,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/arquitectos/:id",
+    component: ArquitectoForm,
+    props: true,
+    meta: { requiresAuth: true },
+  },
 
   { path: "/:pathMatch(.*)*", redirect: "/" },
 ];
@@ -39,12 +73,12 @@ const router = createRouter({
 router.beforeEach((to, _from, next) => {
   const isAuth = !!getAuth();
 
-  if (to.path === "/" && isAuth) {
-    return next("/home");
+  if (to.path === "/home" && isAuth) {
+    return next("/edificios");
   }
 
   if (to.meta.requiresAuth && !isAuth) {
-    return next("/");
+    return next("/home");
   }
 
   next();
